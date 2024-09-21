560ed0a1073adb321cb901d23ae37bf916b95721
María Fernanda Escalona López <mescalona24@alumnos.uaq.mx>
2024-09-21 12:37:05 -0600

    change in Readme

---
+++ cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fer-escalona1/practica-6.git/practica-6/cmd/config"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Variables de entorno: ", config.USERNAME, config.PASSWORD)
+}
